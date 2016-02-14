3b7cac76fa9555a44a6f9120ab8487f657e10943
alex <alex@superpedestrian.com>
2016-02-13 23:27:06 -0500

    Added ability to connect to BLE devices

---
+++ app/src/test/java/com/projectpluto/projectplutoandroid/bluetooth/BleConnectorTest.java
@@ -0,0 +1,98 @@
+package com.projectpluto.projectplutoandroid.bluetooth;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
+
+import com.projectpluto.projectplutoandroid.core.BusProvider;
+import com.squareup.otto.Bus;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(BusProvider.class)
+public class BleConnectorTest extends TestCase {
+    @Mock Bus bus;
+
+    protected void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        PowerMockito.mockStatic(BusProvider.class);
+        PowerMockito.when(BusProvider.getInstance()).thenReturn(bus);
+    }
+
+    @Test
+    public void testRegisterOnCreate() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        verify(bus, times(1)).register(connector);
+    }
+
+    @Test
+    public void testProduceDevices() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        BleConnector.BleDevicesChangedEvent event = connector.produceCurrentConnections();
+        assertEquals(event.connectedGatts, connector.mConnectedDevices);
+    }
+
+    @Test
+    public void testConnect() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        BluetoothDevice device = mock(BluetoothDevice.class);
+        connector.connect(device, false);
+        verify(device, times(1)).connectGatt(Mockito.eq(connector.mContext),
+                Mockito.eq(false),
+                any(BluetoothGattCallback.class));
+    }
+
+    @Test
+    public void testDisconnect() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        BluetoothGatt gatt = mock(BluetoothGatt.class);
+        connector.disconnect(gatt);
+        verify(gatt, times(1)).disconnect();
+    }
+
+    @Test
+    public void testConnectionStateChangeConnected() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        BluetoothGatt gatt = mock(BluetoothGatt.class);
+        connector.mConnectionListener.onConnectionStateChange(gatt, 0, BluetoothProfile.STATE_CONNECTED);
+        assertEquals(connector.mConnectedDevices.size(), 1);
+        assertTrue(connector.mConnectedDevices.contains(gatt));
+
+        ArgumentCaptor<BleConnector.BleDevicesChangedEvent> captor =
+                ArgumentCaptor.forClass(BleConnector.BleDevicesChangedEvent.class);
+        verify(bus).post(captor.capture());
+        assertEquals(connector.mConnectedDevices, captor.getValue().connectedGatts);
+    }
+
+    @Test
+    public void testConnectionStateChangeDisconnected() throws Exception {
+        BleConnector connector = new BleConnector(mock(Context.class));
+        BluetoothGatt gatt = mock(BluetoothGatt.class);
+        connector.mConnectedDevices.add(gatt);
+        connector.mConnectionListener.onConnectionStateChange(gatt, 0, BluetoothProfile.STATE_DISCONNECTED);
+        assertEquals(connector.mConnectedDevices.size(), 0);
+
+        ArgumentCaptor<BleConnector.BleDevicesChangedEvent> captor =
+                ArgumentCaptor.forClass(BleConnector.BleDevicesChangedEvent.class);
+        verify(bus).post(captor.capture());
+        assertEquals(connector.mConnectedDevices, captor.getValue().connectedGatts);
+    }
+}
