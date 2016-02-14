c31e0aa44c385eea2b147889b8a4709b5b19d4c0
alex <alex@superpedestrian.com>
2016-02-14 00:10:49 -0500

    Added powermock and additional tests

--- app/src/test/java/com/projectpluto/projectplutoandroid/bluetooth/BluetoothServiceTest.java
+++ app/src/test/java/com/projectpluto/projectplutoandroid/bluetooth/BluetoothServiceTest.java
@@ -1,25 +1,33 @@
 package com.projectpluto.projectplutoandroid.bluetooth;
 
+import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.content.Context;
 import android.content.Intent;
 
+import com.projectpluto.projectplutoandroid.core.Permissions;
+
 import junit.framework.TestCase;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Permissions.class)
 public class BluetoothServiceTest extends TestCase {
-    @Mock BluetoothManager mBtManager;
-    @Mock BluetoothAdapter mBtAdapter;
-    @Mock Context mContext;
-    @Mock BluetoothLeScanner mAndroidScanner;
 
     @Test
     public void testOnBind() throws Exception {
@@ -45,5 +53,37 @@ public class BluetoothServiceTest extends TestCase {
         btService.stopBleScan();
 
         verify(scanner, times(1)).stopBleScan();
+    }
+
+    @Test
+    public void testStartCommandWithPermission() throws Exception {
+        PowerMockito.mockStatic(Permissions.class);
+        PowerMockito.when(Permissions.hasCoarseLocation(any(Context.class))).thenReturn(true);
+
+        BluetoothService btService = mock(BluetoothService.class);
+        BluetoothManager manager = mock(BluetoothManager.class);
+        BluetoothAdapter adapter = mock(BluetoothAdapter.class);
+
+        when(btService.getSystemService(Context.BLUETOOTH_SERVICE)).thenReturn(manager);
+        when(manager.getAdapter()).thenReturn(adapter);
+
+        doCallRealMethod().when(btService).onStartCommand(null, 0, 0);
+        int result = btService.onStartCommand(null, 0, 0);
+
+        assertEquals(Service.START_STICKY, result);
+        assertNotNull(btService.mBleScanner);
+    }
+
+    @Test
+    public void testStartCommandNoPermission() throws Exception {
+        PowerMockito.mockStatic(Permissions.class);
+        PowerMockito.when(Permissions.hasCoarseLocation(any(Context.class))).thenReturn(false);
+
+        BluetoothService btService = mock(BluetoothService.class);
+        doCallRealMethod().when(btService).onStartCommand(null, 0, 0);
+        int result = btService.onStartCommand(null, 0, 0);
+
+        verify(btService, times(1)).stopSelf();
+        assertEquals(Service.START_NOT_STICKY, result);
     }
 }
---
+++ app/src/test/java/com/projectpluto/projectplutoandroid/core/PermissionsTest.java
@@ -0,0 +1,38 @@
+package com.projectpluto.projectplutoandroid.core;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ContextCompat.class)
+public class PermissionsTest extends TestCase {
+
+    @Test
+    public void testHasPermission() throws Exception {
+        PowerMockito.mockStatic(ContextCompat.class);
+        PowerMockito.when(ContextCompat.checkSelfPermission(any(Context.class), anyString()))
+                .thenReturn(PackageManager.PERMISSION_GRANTED);
+        assertTrue(Permissions.hasCoarseLocation(mock(Context.class)));
+    }
+
+    @Test
+    public void testDoesntHavePermission() throws Exception {
+        PowerMockito.mockStatic(ContextCompat.class);
+        PowerMockito.when(ContextCompat.checkSelfPermission(any(Context.class), anyString()))
+                .thenReturn(PackageManager.PERMISSION_DENIED);
+        assertFalse(Permissions.hasCoarseLocation(mock(Context.class)));
+    }
+}
