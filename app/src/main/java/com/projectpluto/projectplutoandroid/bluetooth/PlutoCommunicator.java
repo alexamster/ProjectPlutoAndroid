2bd72c4887bc6b989dad3a5799a2449915f90794
alex <alex@superpedestrian.com>
2016-05-22 17:17:30 -0400

    Added ability to get and set pluto color via BLE

---
+++ app/src/main/java/com/projectpluto/projectplutoandroid/bluetooth/PlutoCommunicator.java
@@ -0,0 +1,126 @@
+package com.projectpluto.projectplutoandroid.bluetooth;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.support.annotation.Nullable;
+
+import com.projectpluto.projectplutoandroid.core.BusProvider;
+import com.projectpluto.projectplutoandroid.models.PlutoColor;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Produce;
+import com.squareup.otto.Subscribe;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+import timber.log.Timber;
+
+/**
+ * PlutoCommunicator is responsible for listening to changes in the color characteristic and
+ * broadcasting updates to the rest of the app in the form of ColorUpdateEvent.
+ *
+ * It also can write new colors to a Pluto board, and trigger reads if the color has changed.
+ */
+public class PlutoCommunicator {
+    protected static final UUID PLUTO_COLOR_SERVICE = UUID.fromString("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
+    protected static final UUID PLUTO_COLOR_CHAR = UUID.fromString("6E400002-B5A3-F393-E0A9-E50E24DCCA9E");
+
+    protected BleCommunicator mCommunicator;
+    protected Bus mBus = BusProvider.getInstance();
+    protected Set<BluetoothGatt> mPlutoGatts = new HashSet<>();
+
+    @Nullable
+    protected PlutoColor mCurrentColor;
+
+    public PlutoCommunicator(BleCommunicator communicator) {
+        mCommunicator = communicator;
+        mBus.register(PlutoCommunicator.this);
+    }
+
+    @Subscribe
+    public void onBleRead(BleCommunicator.CharacteristicReadEvent event) {
+        if (event.characteristic.getUuid() == PLUTO_COLOR_CHAR) {
+            PlutoColor color = new PlutoColor(event.characteristic.getValue());
+            mCurrentColor = color;
+            mBus.post(new ColorUpdateEvent(color));
+        }
+    }
+
+    @Subscribe
+    public void onBleDevicesChanged(BleConnector.BleDevicesChangedEvent event) {
+        mPlutoGatts = event.connectedGatts;
+    }
+
+    @Produce
+    public ColorUpdateEvent produceCurrentColor() {
+        return new ColorUpdateEvent(mCurrentColor);
+    }
+
+    public void changeColor(PlutoColor color,
+                            BleResultHandler handler) {
+        for (BluetoothGatt gatt : mPlutoGatts) {
+            BluetoothGattCharacteristic bleChar = getGattChar(gatt);
+            if (null == bleChar) {
+                // failure will already be logged in getGattChar(...), no need to do it twice
+                return;
+            }
+
+            mCommunicator.enqueueBleRequest(new BleRequest(BleRequest.RequestType.WRITE,
+                    gatt,
+                    bleChar,
+                    color.getBytes(),
+                    handler));
+        }
+    }
+
+    /**
+     * Handler result indicates the status of the read request (it does not contain the result).
+     * The color will be broadcast via a ColorUpdateEvent once the characteristic is actually read.
+     */
+    public void readColor(BleResultHandler handler) {
+        for (BluetoothGatt gatt : mPlutoGatts) {
+            BluetoothGattCharacteristic bleChar = getGattChar(gatt);
+            if (null == bleChar) {
+                // failure will already be logged in getGattChar(...), no need to do it twice
+                return;
+            }
+
+            mCommunicator.enqueueBleRequest(new BleRequest(BleRequest.RequestType.READ,
+                    gatt,
+                    bleChar,
+                    null,
+                    handler));
+
+            // only get color from one device as they all will be in sync (or will be as soon as
+            // we send a color)
+            return;
+        }
+    }
+
+    public BluetoothGattCharacteristic getGattChar(BluetoothGatt gatt) {
+        BluetoothGattService service = gatt.getService(PLUTO_COLOR_SERVICE);
+        if (service == null) {
+            Timber.e("Unable to find pluto service on device %s", gatt.getDevice().getName());
+            return null;
+        }
+
+        BluetoothGattCharacteristic bleChar = service.getCharacteristic(PLUTO_COLOR_CHAR);
+        if (bleChar == null) {
+            Timber.e("Unable to find pluto service on device %s", gatt.getDevice().getName());
+            return null;
+        }
+
+        return bleChar;
+    }
+
+    public static class ColorUpdateEvent {
+        @Nullable
+        public final PlutoColor color;
+
+        public ColorUpdateEvent(@Nullable PlutoColor color) {
+            this.color = color;
+        }
+    }
+}
---
+++ app/src/test/java/com/projectpluto/projectplutoandroid/models/PlutoColorTest.java
@@ -0,0 +1,54 @@
+package com.projectpluto.projectplutoandroid.models;
+
+import android.graphics.Color;
+
+import com.projectpluto.projectplutoandroid.BuildConfig;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricGradleTestRunner.class)
+// robolectric is needed in order to use android.graphics.Color
+@Config(constants = BuildConfig.class, sdk = 21)
+public class PlutoColorTest extends TestCase {
+    @Test
+    public void testFromBytes() throws Exception {
+        byte[] data = new byte[4];
+        data[0] = PlutoColor.COLOR_COMMAND;
+        data[1] = 11;
+        data[2] = 22;
+        data[3] = 33;
+        PlutoColor color = new PlutoColor(data);
+        assertEquals(color.getBytes(), data);
+
+        int androidColor = color.getAndroidColor();
+        assertEquals(Color.red(androidColor), data[1]);
+        assertEquals(Color.green(androidColor), data[2]);
+        assertEquals(Color.blue(androidColor), data[3]);
+    }
+
+    @Test
+    public void testFromRgb() throws Exception {
+        PlutoColor color = new PlutoColor(11, 0, 33);
+        byte[] data = color.getBytes();
+
+        assertEquals(data[0], PlutoColor.COLOR_COMMAND);
+        assertEquals(data[1], 11);
+        assertEquals(data[2], 0);
+        assertEquals(data[3], 33);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromRgbInvalidRangeLarge() throws Exception {
+        PlutoColor color = new PlutoColor(11, 22, 400);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromRgbInvalidRangeNegative() throws Exception {
+        PlutoColor color = new PlutoColor(11, 22, -20);
+    }
+}
